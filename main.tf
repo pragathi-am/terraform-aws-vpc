e8094a82ddf80accfb48708b42e94b5055dd1117
prag <pragathiraga@gmail.com>
2026-03-03 22:01:36 +0530

    foreach ex

--- main.tf
+++ main.tf
@@ -4,5 +4,4 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = local.vpc_final_tags
-     
 }
--- variables.tf
+++ variables.tf
@@ -12,6 +12,6 @@ variable "vpc_cidr" {
 }
 
 variable "vpc_tags" {
-  type = map 
-  default = {}
-  }
+    type = map
+    default = {}
+}
