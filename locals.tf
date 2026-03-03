56ab4743945516bb6f478fac8d220b263d10b721
prag <pragathiraga@gmail.com>
2026-03-03 20:36:00 +0530

    foreach ex

--- locals.tf
+++ locals.tf
@@ -3,7 +3,12 @@ locals {
       Project = var.project
       Environment = var.environment
       Terraform = true
-
    }
+   vpc_final_tags = merge (
+        local.common_tags,
+        {
+         Name = {var.project_name}-{var.environment}
+        },
+        var.vpc_tags
 
 }
