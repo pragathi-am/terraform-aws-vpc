b9b58282f4ebb041fc27a044e4489f345687c23d
prag <pragathiraga@gmail.com>
2026-03-03 20:39:06 +0530

    foreach ex

--- locals.tf
+++ locals.tf
@@ -2,12 +2,12 @@ locals {
    common_tags = {
       Project = var.project
       Environment = var.environment
-      Terraform = true
+      Terraform = "true"
    }
    vpc_final_tags = merge (
         local.common_tags,
         {
-         Name = {var.project_name}-{var.environment}
+         Name = "${var.project_name}-${var.environment}"
         },
         var.vpc_tags
    )
