295b01b5f8b3d06e16660e3d76181d0142b04330
prag <pragathiraga@gmail.com>
2026-03-04 20:57:53 +0530

    foreach ex

--- main.tf
+++ main.tf
@@ -7,7 +7,17 @@ resource "aws_vpc" "main" {       # 1. create VPC
 }
 
 resource "aws_internet_gateway" "main" {  # 2. create internet gateway and attach to VPC
-  vpc_id = aws_vpc.main.id  # attach CPC
+  vpc_id = aws_vpc.main.id  # attach VPC
   tags = local.igw_final_tags    
   
 }
+
+/* 
+resource "aws_subnet" "main" {   # 3. subnet creation
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+} */
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "aws_zones" {
+   value = data.aws_availability_zones.available
+}
