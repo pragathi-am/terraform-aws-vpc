5e045970081d542163dfd550055099b5616a4aa1
prag <pragathiraga@gmail.com>
2026-03-05 11:15:44 +0530

    foreach ex

--- main.tf
+++ main.tf
@@ -12,8 +12,9 @@ resource "aws_internet_gateway" "main" {  # 2. create internet gateway and attac
   
 }
 
-# 3. subnet creation
-resource "aws_subnet" "main" {   
+# 3. subnet creation 
+# public subnet
+resource "aws_subnet" "public" {   
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
@@ -29,3 +30,91 @@ resource "aws_subnet" "main" {
      )
    
   }
+  
+  # private subnet
+  resource "aws_subnet" "private" {   
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  
+  tags = merge (
+         local.common_tags ,
+         { # roboshop-dev-private-us-east-1a
+           Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+         },
+         var.private_subnet_tags 
+     )
+   
+  }
+# database subnet
+  resource "aws_subnet" "database" {   
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  
+  tags = merge (
+         local.common_tags ,
+         { # roboshop-dev-database-us-east-1a
+           Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+         },
+         var.database_subnet_tags 
+     )
+   
+  }
+
+  # 4. route table creation 
+  # public route table
+  resource "aws_route_table" "public" {
+        vpc_id = aws_vpc.main.id
+
+        route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.example.id
+        }
+
+        tags = merge (
+                local.common_tags ,
+                { # roboshop-dev-public
+                   Name = "${var.project}-${var.environment}-public"
+                },
+                var.public_route_table_tags 
+            )
+    }
+
+    # private route table
+  resource "aws_route_table" "private" {
+        vpc_id = aws_vpc.main.id
+
+        route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.example.id
+        }
+
+        tags = merge (
+                local.common_tags ,
+                { # roboshop-dev-private
+                   Name = "${var.project}-${var.environment}-private"
+                },
+                var.private_route_table_tags 
+            )
+    }  
+
+        # database route table
+  resource "aws_route_table" "database" {
+        vpc_id = aws_vpc.main.id
+
+        route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.example.id
+        }
+
+        tags = merge (
+                local.common_tags ,
+                { # roboshop-dev-database
+                   Name = "${var.project}-${var.environment}-database"
+                },
+                var.database_route_table_tags 
+            )
+    }  
--- variables.tf
+++ variables.tf
@@ -41,3 +41,28 @@ variable "public_subnet_tags" {
     type = map
     default = {}
 }
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
